e43f0e2580a2efa8e23bf180faf8acf070fc1aa7
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-04-09 15:36:33 -0500

    Create main.cpp

---
+++ tareas/tarea5/punto1-2/main.cpp
@@ -0,0 +1,55 @@
+#include "lista.h"
+#include <iostream>
+
+// Punto 1
+int contarOcurrencias(Lista &l, int v)
+{
+    int cont = 0;
+    for (int i = 1; i <= l.longLista(); i++)
+    {
+        if (l.infoLista(i) == v)
+        {
+            cont++;
+        }
+    }
+    return cont;
+}
+
+// Punto 2
+
+Lista obtenerMenores(Lista &l, int v)
+{
+    Lista ans;
+    for (int i = 1; i <= l.longLista(); i++)
+    {
+        if (l.infoLista(i) < v)
+        {
+            ans.anxLista(l.infoLista(i));
+        }
+    }
+    return ans;
+}
+
+void printLista(Lista &l)
+{
+    for (int i = 1; i <= l.longLista(); i++)
+    {
+        cout << l.infoLista(i) << endl;
+    }
+}
+
+int main()
+{
+    Lista list;
+    list.anxLista(0);
+    list.anxLista(2);
+    list.anxLista(5);
+    list.anxLista(3);
+    list.anxLista(2);
+    list.anxLista(4);
+    int ans = contarOcurrencias(list, 2);
+    cout << ans << endl
+         << endl;
+    Lista result = obtenerMenores(list, 4);
+    printLista(result);
+}
