b4e76d22e5e74ba2b6fee9a5bed38800a031eb00
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-04-19 16:34:20 -0500

    Create lista.h

---
+++ tareas/tarea5/punto4/b/lista.h
@@ -0,0 +1,45 @@
+#ifndef __LISTA_H
+#define __LISTA_H
+
+#include <iostream>
+
+using namespace std;
+
+/************************
+ * ESTRUCTURAS DE DATOS *
+ ************************/
+typedef int Elemento;
+
+class Nodo
+{
+public:
+    Elemento dato;
+    Nodo *sig;
+};
+
+class Lista
+{
+private:
+    Nodo *act;
+
+public:
+    /************************
+     * OPERACIONES DEL TAD *
+     ************************/
+
+    /* Constructoras */
+    Lista();
+
+    /* Modificadoras */
+    void anxLista(Elemento elem);
+    void insLista(Elemento elem, int pos);
+    void elimLista(int pos);
+    Lista operator+(Lista &est);
+
+    /* Analizadoras */
+    Elemento infoLista(int pos);
+    int longLista();
+    bool vaciaLista();
+};
+
+#endif
