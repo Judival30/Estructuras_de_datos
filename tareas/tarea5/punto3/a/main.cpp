89fadd7331c9bc5e48f7acc61e967a031e4df29a
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-04-19 17:17:38 -0500

    Update main.cpp

--- tareas/tarea5/punto3/a/main.cpp
+++ tareas/tarea5/punto3/a/main.cpp
@@ -1,9 +1,28 @@
+/*
+Estructuras de datos - tarea5 - punto 3a
+Juan Diego Valencia Alomia 
+code: 8977467
+
+Para el desarrollo de este punto se escogió la implementación creada por el docente Carlos Ramirez de una lista simple, y se implementaron las funciones
+sin modificar la interfaz del TAD:
+
+Complejidad:
+La función insertarEnListaOrdenada hace uso de la función infoLista() como punto de parada para encontrar la posición donde se debe insertar, por lo que
+llamaremos k al numero que corresponde a la posición donde se debe insertar v, además se usa longLista para saber el tamaño de la lista y finalmente 
+la función de insertar que tiene complejidad k, por lo tanto T(n) = n + 2k, por lo tanto demuestra un comportamiento lineal, es decir que su complejidad
+es O(n)
+
+Explicación de uso de las funciones:
+Se hace uso del procedimiento printlist() el cual es usado para imprimir la lista. Entonces se crea en el main una lista y se llama a las dos funciones 
+analizadoras que acabamos de crear y se comprueba su funcionalidad.
+*/
 #include "lista.h"
 
 void insertarEnListaOrdenada(Lista &l, int v)
 {
     int ins = 0;
-    for (int i = 1; i <= l.longLista() and l.infoLista(i) < v; i++)
+    int size = l.longLista();
+    for (int i = 1; i <= size and l.infoLista(i) < v; i++)
     {
         ins = i + 1;
     }
