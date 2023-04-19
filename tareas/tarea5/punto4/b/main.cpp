c07d20639afe507e13a2d1a8dd41e373a229f8f1
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-04-19 18:06:56 -0500

    Update main.cpp

--- tareas/tarea5/punto4/b/main.cpp
+++ tareas/tarea5/punto4/b/main.cpp
@@ -1,3 +1,22 @@
+/*
+Estructuras de datos - tarea5 - punto 4b
+Juan Diego Valencia Alomia 
+code: 8977467
+
+Para el desarrollo de este punto se escogió la implementación creada por el docente Carlos Ramirez de una lista simple, y se implementaron una sobrecarga
+del + para sumar listas
+
+Complejidad:
+La sobrecargar del + tiene dos momentos, el primer momento recorre la lista 1 por medio de sus nodos y se hace uso de la función anxlista para agregar
+los elementos de la lista 1 a la lista ans, por lo que se genera una sumatoria de 0 a n siendo n el tamaño de l1 ya que se mofica en cada iteración el 
+tamaño de la lista ans, dando un comportamiento cuadratico. En el segundo momento es algo similar pero esta vez son dos sumatorias con respecto al recorrido
+de la lista ans con anxlista y luego el recorrido de la lista est con info lista, por lo que tambien es un comportamiento cudratico. De esta manera se 
+puede concluir que la complejidad es O(n^2)
+
+Explicación de uso de las funciones:
+Se hace uso del procedimiento printlist() el cual es usado para imprimir la lista. Entonces se crea en el main dos listas l1 y l2
+y posteriormente se crea una tercera l3 a la cual se le asigna la suma de l1 + l2 y se comprueba su funcionalidad.
+*/
 #include "lista.h"
 
 void printLista(Lista &l)
