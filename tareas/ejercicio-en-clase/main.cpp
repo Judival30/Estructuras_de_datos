66e8aab258e09c6174c7ca8c4c0c036c1c39ab53
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-05-05 12:42:20 -0500

    Update main.cpp

--- tareas/ejercicio-en-clase/main.cpp
+++ tareas/ejercicio-en-clase/main.cpp
@@ -1,3 +1,26 @@
+/*
+Autor: Juan Diego Valencia Alomia
+5/05/2023
+Implementación del TAD cola de prioridad
+
+Complejidad de los metodos:
+-front():
+    Tiene una complejidad de O(1) ya que se obtiene el iterador en la primera posición con begin() y se retorna lo que se 
+    encuentra dentro de dicho iterador.
+-enqueue():
+    Tiene una complejidad de nlog(n) siendo n el tamaño de la lista, ya que esta es la complejidad de organizar la lista interna
+    por medio del sort.
+-deque():
+    Tiene una complejidad de O(1) ya que eliminar el primer elemento de la lista doble-circ es constante
+-vaciaCola():
+    Tiene una complejidad de O(1) ya que la función empty() tan solo verifica si la lista está vacía, lo cual realiza en tiempo 
+    constante
+
+Cabe resaltar que se podría optimizar la funcion de enqueue ya que se podría usar monticulos (heap), la cual tiene una menor complejidad.
+
+*/
+
+
 #include "cola-prioridad.h"
 
 void imprimirElementos(Cola &c)
