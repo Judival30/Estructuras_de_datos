735d17494d3cf751ce4ce0f9c83880fabe0b6172
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2023-05-06 14:47:27 -0500

    Add files via upload

--- proyecto/bigInteger.h
+++ proyecto/bigInteger.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -13,21 +14,40 @@ private:
     int sign;
 
 public:
+    // Constructores
+    BigInteger();
     BigInteger(string n);
+    BigInteger(BigInteger &n);
     BigInteger(vector<int> n, int s);
 
+    // Operaciones que cambían el actual
     void add(BigInteger &est);
     void substract(BigInteger &est);
     void product(BigInteger &est);
+    void quotient(BigInteger &est);
+    void remainder(BigInteger &est);
+    void remainder(BigInteger &est);
+    void pow(BigInteger &est);
 
+    // Sobrecargas de los operadores
     BigInteger operator+(BigInteger &est);
     BigInteger operator-(BigInteger &est);
     BigInteger operator*(BigInteger &est);
+    BigInteger operator/(BigInteger &est);
+    BigInteger operator%(BigInteger &est);
+    bool operator==(BigInteger &est);
+    bool operator<=(BigInteger &est);
+    bool operator<(BigInteger &est);
 
+    // Operaciones analizadoras
+    string toString();
     int sizeInt();
     int infoNum(int v);
     int getSign();
     void printBigInt();
+
+    BigInteger sumarListaValores(list<BigInteger> &n);
+    BigInteger MultiplicarListaValores(list<BigInteger> &n);
 };
 
 #endif
